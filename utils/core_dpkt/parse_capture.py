18eb5d22a8299beb9d4d28ab0381d8badf2c205e
Randark <randark@foxmail.com>
2024-10-29 20:55:52 +0800

    优化流量包文件解析函数

--- utils/core_dpkt/parse_capture.py
+++ utils/core_dpkt/parse_capture.py
@@ -1,56 +1,65 @@
 import dpkt
 import logging
-from typing import cast
+from typing import Union, Type, cast
 from rich.progress import Progress
 
 
-def parse_pcap_file(pcap_file: str) -> tuple[int, list[tuple[float, bytes]]]:
-    traffic_data: list[tuple[float, bytes]] = []
+def _extract_packets(
+    pcap_file: str,
+    reader_class: Union[Type[dpkt.pcap.Reader], Type[dpkt.pcapng.Reader]],
+) -> tuple[int, list[tuple[float, bytes]]]:
+    """通用解析函数，支持 pcap 和 pcapng 格式
+
+    Args:
+        pcap_file (str): pcap 或 pcapng 文件路径
+        reader_class (dpkt.pcap.Reader | dpkt.pcapng.Reader): Reader 类
+
+    Returns:
+        tuple[int, list[tuple[float, bytes]]]:
+            int: Physical Layer Protocol ID
+            list[tuple[float, bytes]]:
+                - float: packet time
+                - bytes: packet raw data
+    """
+    packets = []
     with Progress() as progress, open(pcap_file, "rb") as _f:
+        # 计算原始包数量
         logging.info("Calculating raw packets quantity...")
-        _count = 0
-        for ts, buf in dpkt.pcap.Reader(_f):
-            _count += 1
+        _count = sum(1 for _ in reader_class(_f))
         logging.info(f"Packets quantity: {_count}")
+        # 重置文件指针
         _f.seek(0, 0)
-        packets_progress = progress.add_task("[green]Scanning for raw packets...", total=_count)
-        for ts, buf in dpkt.pcap.Reader(_f):
-            traffic_data.append((ts, buf))
+        packets_progress = progress.add_task(
+            "[green]Scanning for raw packets...", total=_count
+        )
+        for ts, buf in reader_class(_f):
+            packets.append((ts, buf))
             progress.update(packets_progress, advance=1)
         _f.seek(0, 0)
-        data_link_type = cast(int, dpkt.pcapng.Reader(_f).datalink())
+        data_link_type = cast(int, reader_class(_f).datalink())
     logging.info(f"Physical Layer Protocol ID: {data_link_type}")
-    logging.info(f"Valid raw packets: {len(traffic_data)}")
+    logging.info(f"Valid raw packets: {len(packets)}")
     # ts, bytes
-    return data_link_type, traffic_data
-
-
-def parse_pcapng_file(pcapng_file: str) -> tuple[int, list[tuple[float, bytes]]]:
-    traffic_data: list[tuple[float, bytes]] = []
-    with Progress() as progress, open(pcapng_file, "rb") as _f:
-        logging.info("Calculating raw packets quantity...")
-        _count = 0
-        for ts, buf in dpkt.pcapng.Reader(_f):
-            _count += 1
-        logging.info(f"Packets quantity: {_count}")
-        _f.seek(0, 0)
-        packets_progress = progress.add_task("[green]Scanning for raw packets...", total=_count)
-        for ts, buf in dpkt.pcapng.Reader(_f):
-            traffic_data.append((ts, buf))
-            progress.update(packets_progress, advance=1)
-        _f.seek(0, 0)
-        data_link_type = cast(int, dpkt.pcapng.Reader(_f).datalink())
-    logging.info(f"Physical Layer Protocol ID: {data_link_type}")
-    logging.info(f"Valid raw packets: {len(traffic_data)}")
-    # ts, bytes
-    return data_link_type, traffic_data
+    return data_link_type, packets
 
 
 def parse_capture_file(file_path: str) -> tuple[int, list[tuple[float, bytes]]]:
+    """从 pcap 或 pcapng 文件中解析原始数据包
+
+    Args:
+        pcap_file (str): pcap 或 pcapng 流量包文件
+
+    Returns:
+        tuple[int, list[tuple[float, bytes]]]:
+            int: Physical Layer Protocol ID
+            list[tuple[float, bytes]]:
+                - float: packet time
+                - bytes: packet raw data
+    """
     if file_path.endswith("pcap"):
-        return parse_pcap_file(file_path)
+        return _extract_packets(file_path, dpkt.pcap.Reader)
     elif file_path.endswith("pcapng"):
-        return parse_pcapng_file(file_path)
+        return _extract_packets(file_path, dpkt.pcapng.Reader)
     else:
         logging.error(f"Unsupported file format: {file_path}")
         exit()
